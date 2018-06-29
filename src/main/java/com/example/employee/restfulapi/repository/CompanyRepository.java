ee0ac4e66fa90fe9fac1c8b01cce130cad98a393
Angel-kou <1844646307@qq.com>
2018-06-29 10:33:26 +0800

    complete jpa repository

--- src/main/java/com/example/employee/restfulapi/repository/CompanyRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/CompanyRepository.java
@@ -1,9 +1,22 @@
 package com.example.employee.restfulapi.repository;
 
 import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
+public interface CompanyRepository extends JpaRepository<Company, Long>  {
+
+
+
 }
--- src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
@@ -2,8 +2,20 @@ package com.example.employee.restfulapi.repository;
 
 import com.example.employee.restfulapi.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long>  {
+
+    @Query("select e from Employee e where e.companyId = :companyId")
+    List<Employee> findByCompanyId(@Param("companyId") Long companyId);
+
+
+    @Query("select e from Employee e where e.gender = 'male'")
+    List<Employee> findByGender();
 }
